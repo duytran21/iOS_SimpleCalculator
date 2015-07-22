cfa611175d9e0f28c1c1e2b020f3db5c348c5d81
duytran21 <duytran211189@gmail.com>
2015-07-22 00:36:30 -0700

    Initial Commit

---
+++ SimpleCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleCalculator
+//
+//  Created by Ivan Tran on 7/22/15.
+//  Copyright (c) 2015 Ivan Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
